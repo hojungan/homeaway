99c7b284112739fdda16a2c3ecb7b2644e8fe0e5
Hojung <hojung1111@gmail.com>
2024-08-25 12:41:19 -0400

    Clerk and Toast added to links dropdown

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client";
+
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+import cn from "clsx";
 
 function SignOutLink() {
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({
+      className: cn(
+        "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4"
+      ),
+      description: "You have been signed out.",
+    });
+  };
   return (
-    <div>SignOutLink</div>
-  )
+    <SignOutButton redirectUrl="/">
+      <button className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
 }
-
-export default SignOutLink
+export default SignOutLink;
