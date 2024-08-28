3bfbd61875082a92151bfb7d20ea7fc96e356928
Hojung <hojung1111@gmail.com>
2024-08-27 21:24:59 -0400

    zod implementation to create profile form

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -2,15 +2,11 @@
 
 import { SignOutButton } from "@clerk/nextjs";
 import { useToast } from "../ui/use-toast";
-import cn from "clsx";
 
 function SignOutLink() {
   const { toast } = useToast();
   const handleLogout = () => {
     toast({
-      className: cn(
-        "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4"
-      ),
       description: "You have been signed out.",
     });
   };
