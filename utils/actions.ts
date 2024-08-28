60486a80042b6df7699a6eac664e210ed0f75961
Hojung <hojung1111@gmail.com>
2024-08-28 00:28:37 -0400

    create profile model and createProfileAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,17 +1,39 @@
-'use server';
+"use server";
 
+import db from "./db";
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { profileSchema } from "./schemas";
 
+export const createProfileAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  try {
+    const user = await currentUser();
+    if (!user) throw new Error("User not found");
 
-export const createProfileAction = async (prevState: any, formData: FormData) => {
-    try {
-        const rawData = Object.fromEntries(formData);
-        const validatedFields = profileSchema.parse(rawData);
-        console.log(validatedFields);
-        return { message: "Profile Created" };
-    } catch (error) {
-        console.log(error);
-        return { message: "there was an error..." };
-    }
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+  } catch (error) {
+    return {
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
+  }
+
+  redirect("/");
 };
-  
