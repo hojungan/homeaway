3a7269287d9d2508f0d56d78681da45461cc36eb
Hojung <hojung1111@gmail.com>
2024-08-31 15:12:02 -0400

    fetch properties from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
-import { Button } from "@/components/ui/button"
+import CategoriesList from "@/components/home/CategoriesList";
+import PropertiesContainer from "@/components/home/PropertiesContainer";
+import { Suspense } from "react";
+import LoadingCards from "@/components/card/LoadingCards";
 
-const HomePage = () => {
+const HomePage = ({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) => {
   return (
-    <div>
-      <Button>Button</Button>
-    </div>
-  )
-}
+    <section>
+      <CategoriesList
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams?.category}
+          search={searchParams?.search}
+        />
+      </Suspense>
+    </section>
+  );
+};
 
-export default HomePage
+export default HomePage;
