346d8dcdadfe1cd2d430a544e25f3c9db542f190
Hojung <hojung1111@gmail.com>
2024-08-31 23:00:32 -0400

    share button

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,6 @@
+import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import ShareButton from "@/components/properties/ShareButton";
 import { fetchPropertyDetails } from "@/utils/actions";
 import { redirect } from "next/navigation";
 
@@ -6,7 +9,19 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
   if (!property) redirect("/");
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
-  return <div>PropertiesPage</div>;
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold ">{property.tagline}</h1>
+        <div className="flex items-center gap-x-4">
+          {/* share button */}
+          <ShareButton name={property.name} propertyId={property.id} />
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+    </section>
+  );
 };
 
 export default PropertyDetailsPage;
