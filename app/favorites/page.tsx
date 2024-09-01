1beec647428590ee9bb6c0602d335b144f55e27a
Hojung <hojung1111@gmail.com>
2024-08-31 22:31:29 -0400

    show favorites on favorites page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import EmptyList from "@/components/home/EmptyList";
+import PropertiesList from "@/components/home/PropertiesList";
+import { fetchFavorites } from "@/utils/actions";
 
-const FavoritesPage = () => {
-  return (
-    <div>FavoritesPage</div>
-  )
-}
+const FavoritesPage = async () => {
+  const favorites = await fetchFavorites();
+  if (favorites.length === 0) return <EmptyList />;
+  return <PropertiesList properties={favorites} />;
+};
 
-export default FavoritesPage
+export default FavoritesPage;
